5404bd10854acb31233a46c5b384e535a573d722
syuanbin <syuanbin@gmail.com>
2014-10-10 11:10:40 +0800

    123
    123

--- Exercise/src/com/syuanbin/exercise/MainActivity.java
+++ Exercise/src/com/syuanbin/exercise/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 	}
 }
